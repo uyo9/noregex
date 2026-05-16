eb36dae2545810332ff1be187ad97bb85ae77355
Ren-Yu Lin <uynilo9+dev@proton.me>
2026-05-16 17:03:27 +0800

    feat: add TextStart and TextEnd anchors

--- anchors.go
+++ anchors.go
@@ -2,5 +2,7 @@ package noregex
 
 func LineStart() Pattern       { return pattern{"^"} }
 func LineEnd() Pattern         { return pattern{"$"} }
+func TextStart() Pattern       { return pattern{`\A`} }
+func TextEnd() Pattern         { return pattern{`\z`} }
 func WordBoundary() Pattern    { return pattern{`\b`} }
 func NotWordBoundary() Pattern { return pattern{`\B`} }
